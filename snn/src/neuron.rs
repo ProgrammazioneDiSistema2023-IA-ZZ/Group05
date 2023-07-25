1659150d6004dc37a30273a5a14cdcf0e14bddd0
Your Name <you@example.com>
2023-07-25 15:36:31 +0200

    implementato Default per Neuron; aggiornati campi struct Neuron; aggiornati campi struct Synapsis; aggiornato costruttori Synapsis e Neuron;

--- snn/src/neuron.rs
+++ snn/src/neuron.rs
@@ -1,40 +1,52 @@
-
+use std::sync::Arc;
 pub struct Neuron{
-    v_mem :f64, //configurabile
+    v_mem :f64,
     v_th :f64, //configurabile
     v_rest :f64, //configurabile
-    output :bool
+    v_reset :f64, //configurabile
+    tau :f64, //configurabile
+    //output :bool
 }
 
 pub struct Synapsis{
-    weight:f64,
-    propagation_time:f64
+    weight: f64,
+    source_neuron: Arc<Neuron>,
+    dest_neuron: Arc<Neuron>,
+}
+
+impl Default for Neuron {
+    fn default() -> Self{
+        Neuron{
+            v_mem: -7e-2,
+            v_th: -5.5e-2,
+            v_rest: -7e-2,
+            v_reset: -7e-2,
+            tau: 1e-2,
+            //output:false
+        }
+    }
 }
 
 impl Neuron{
-    pub fn default() -> Self{
+    
+    pub fn new(v_th: f64, v_rest: f64, v_reset: f64, tau: f64) -> Self{
         Neuron{
-            v_mem:-7e-2,
-            v_th:-5.5e-2,
-            v_rest:-7e2,
-            output:false
-        }
-    }
-    pub fn new(v_mem:f64,v_th:f64,v_rest:f64) -> Self{
-        Neuron{
-            v_mem,
-            v_th,
-            v_rest,
-            output:false
+            v_mem: v_rest,
+            v_th: v_th,
+            v_rest: v_rest,
+            v_reset: v_reset,
+            tau: tau,
+            //output:false
         }
     }
 }
 
 impl Synapsis{
-    pub fn new() -> Self{
+    pub fn new(weight: f64, source_neuron: Arc<Neuron>, dest_neuron: Arc<Neuron>) -> Self{
         Synapsis{
-            weight:1.0,
-            propagation_time:0.3e-3
+            weight: weight,
+            source_neuron: source_neuron,
+            dest_neuron: dest_neuron,
         }
     }
 }
