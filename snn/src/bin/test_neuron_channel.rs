89e3ecfff8f48a56f7489c766aafa95e54291bf6
Your Name <you@example.com>
2023-07-28 15:21:01 +0200

    implementato loop del neurone; modifiche minori;

--- snn/src/bin/test_neuron_channel.rs
+++ snn/src/bin/test_neuron_channel.rs
@@ -9,7 +9,7 @@ async fn main() {
 
     let parameters = neuron::NeuronParameters::default();
 
-    let mut node = neuron::Neuron::new(0, parameters);
+    let mut node = neuron::Neuron::new(0, 0, parameters);
     let channel = node.get_channel();
     let h1 = tokio::spawn(async move {
         node.run().await;
@@ -18,7 +18,7 @@ async fn main() {
     let h2 = tokio::spawn(async move {
         loop {
             tokio::time::sleep(Duration::from_secs(3)).await;
-            let _ = channel.send(Pulse {}).await.unwrap();
+            let _ = channel.send(Pulse::new(1.0)).await.unwrap();
         }
     });
 
